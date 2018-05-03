54c46a5af34973b021348384f38fe27cbbe96882
Diana <33287508+VeronaDi@users.noreply.github.com>
2018-05-03 20:28:16 +0300

    new

---
+++ index.js
@@ -0,0 +1,42 @@
+import './styles/main.scss'
+
+import Glide from '@glidejs/glide'
+
+import AOS from 'aos'
+
+import $ from 'jquery'
+
+new Glide('.js-glide').mount()
+AOS.init()
+
+$(document).ready(function () {
+  // Smooth scroll
+  $('.js-smooth-scroll').click(function () {
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 1000)
+  })
+
+  // Back to top
+  function topFunction () {
+    $('html, body').stop().animate({
+      scrollTop: 0
+    }, 500)
+  }
+  $('.js-to-top').on('click', topFunction)
+})
+
+$(document).on('scroll', function (ev) {
+  var scrollOffset = $(document).scrollTop()
+
+  if (scrollOffset > 600) {
+    $('.js-to-top').addClass('visible')
+  } else {
+    $('.js-to-top').removeClass('visible')
+  }
+})
+
+// Mobile Navi HideShow Button //
+$('.js-show-mob-menu').on('click', function (event) {
+  $('.mobile-nav__list').toggleClass('show')
+})
